4e1009dd3bbb438e8bff1bebdc7dfe4ba120f77c
MahmoudNassim <mahmodnassim7@gmail.com>
2025-01-13 05:36:34 +0200

    first slide of section one at home page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,50 @@
-var textWrapper = document.querySelector(".ml10 .letters");
-textWrapper.innerHTML = textWrapper.textContent.replace(
+var firstTopTextWrapper = document.querySelector(".first-slide .letters");
+firstTopTextWrapper.innerHTML = firstTopTextWrapper.textContent.replace(
   /\S/g,
   "<span class='letter'>$&</span>"
 );
 
-anime
-  .timeline({ loop: true })
-  .add({
-    targets: ".ml10 .letter",
+var firstBottomTextWrapper = document.querySelector(".second-slide .letters");
+firstBottomTextWrapper.innerHTML = firstBottomTextWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+
+var sameAnimationWrappers = document.querySelectorAll(
+  ".same-animation .letters"
+);
+sameAnimationWrappers.forEach((wrapper) => {
+  wrapper.innerHTML = wrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+  );
+});
+
+anime.timeline().add({
+  targets: ".first-slide .letter",
+  opacity: [0, 1],
+  translateX: [40, 0.5],
+  easing: "easeInOutBack",
+  duration: 500,
+  delay: (el, i) => 30 * i,
+});
+
+anime.timeline().add({
+  targets: ".second-slide .letter",
+  opacity: [0, 1],
+  translateX: [-40, -0.5],
+  easing: "easeInOutBack",
+  duration: 500,
+  delay: (el, i) => 30 * i,
+});
+
+sameAnimationWrappers.forEach((wrapper) => {
+  anime.timeline().add({
+    targets: wrapper.querySelectorAll(".letter"),
+    opacity: [0, 1],
     rotateY: [-90, 0],
     duration: 1300,
     delay: (el, i) => 45 * i,
-  })
-  .add({
-    targets: ".ml10",
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000,
   });
-var textWrapper = document.querySelector(".ml12");
-textWrapper.innerHTML = textWrapper.textContent.replace(
-  /\S/g,
-  "<span class='letter'>$&</span>"
-);
-
-anime
-  .timeline({ loop: true })
-  .add({
-    targets: ".ml12 .letter",
-    translateX: [40, 0],
-    translateZ: 0,
-    opacity: [0, 1],
-    easing: "easeOutExpo",
-    duration: 1200,
-    delay: (el, i) => 500 + 30 * i,
-  })
-  .add({
-    targets: ".ml12 .letter",
-    translateX: [0, -30],
-    opacity: [1, 0],
-    easing: "easeInExpo",
-    duration: 1100,
-    delay: (el, i) => 100 + 30 * i,
-  });
+});
